5c42b343581797d4be87ec2e57a4e78120e3b653
abdelmac <abdelkadermachta@gmail.com>
2024-02-11 02:11:55 +0100

    effeef

--- hikma/App/Components/CourseList.js
+++ hikma/App/Components/CourseList.js
@@ -1,34 +1,51 @@
+import { View, Text } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import GlobalApi from '../Shared/GlobalApi'
 import { FlatList } from 'react-native';
 import { Image } from 'react-native';
 import Colors from '../Shared/Colors';
 import { TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
-export default function CourseList({type}) {
 export default function CourseList({type}) { 
     const [courseList,setCourseList]=useState([])
     const navigation=useNavigation();
     useEffect(()=>{
-        
+         
         getCourseList();
+    },[])
+
+    const getCourseList=async()=>{
+        const resp=(await GlobalApi.getCourseList(type)).data;
+        const result=resp.data.map((item)=>({
+            id:item.id,
+            name:item.attributes.name,
+            description:item.attributes.description,
+            image:item.attributes.image.data.attributes.url,
+            Topic:item.attributes.Topic
+
+        }))
        
         setCourseList(result); 
     }
 
     const onPressCourse=(course)=>{
         
-        navigation.navigate('course-detail',{courseData:course})
+        navigation.navigate('course-detail',{courseData:course,
+          courseType:'text'})
     }
   return (
     <View style={{marginTop:10}}>
     <Text style={{fontSize:20,fontWeight:'bold' ,
+    textTransform:'capitalize',
+    marginBottom:3}}>
+        {type} Course</Text>
+
+      <FlatList
+      data={courseList}
       horizontal={true}
       showsHorizontalScrollIndicator={false}
       renderItem={({item})=>(
-        <View style={{backgroundColor:Colors.white,marginRight:10,
-        borderRadius:10}}>
-            <Image source={{uri:item.image}} 
-            style={{width:180,height:80, borderRadius:10}} />
         <TouchableOpacity style={{backgroundColor:Colors.white,marginRight:10,
         borderRadius:10}} onPress={()=>onPressCourse(item)}>
             <Image source={{uri:item.image}}  
@@ -41,8 +58,9 @@ export default function CourseList({type}) {
 
             </View>
            
-        </View> 
         </TouchableOpacity> 
       )}
       />
     </View>
+  )
+}
--- hikma/App/Components/VideoCourseList.js
+++ hikma/App/Components/VideoCourseList.js
@@ -1,3 +1,6 @@
+import { View, Text } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import GlobalApi from '../Shared/GlobalApi'
 import { FlatList } from 'react-native';
 import { Image } from 'react-native';
 import { TurboModuleRegistry } from 'react-native';
@@ -12,39 +15,38 @@ export default function VideoCourseList() {
     useEffect(()=>{
         getVideoCourse();
     },[])
+    const getVideoCourse=async()=>{
         const resp=(await GlobalApi.getVideoCourse()).data;
         const result=resp.data.map((item)=>({
             id:item.id,
-            title:item.attributes.title,
-            desc:item.attributes.description,
             name:item.attributes.title,
             description:item.attributes.description,
             image:item.attributes.image.data.attributes.url,
-            videoTopic:item.attributes.VideoTopic
             Topic:item.attributes.VideoTopic
         }))
         setVideoList(result);
-        console.log(result)
         console.log(result) 
     }
     const onPressCourse=(course)=>{
         
-        navigation.navigate('course-detail',{courseData:course})
+        navigation.navigate('course-detail',{courseData:course,
+        courseType:'video'})
     }
   return (
     <View style={{marginTop:15}}>
+        <Text style={{fontSize:20,fontWeight:'bold',marginBottom:3}}>Video Course</Text>
+     <FlatList
+     data={videoList}
      horizontal={true}
      showsHorizontalScrollIndicator={false}
      renderItem={({item})=>(
-        <View>
         <TouchableOpacity onPress={()=>onPressCourse(item)}>
             <Image source={{uri:item.image}} 
-            style={{width:180,height:100,
-            marginRight:10,borderRadius:7}} />
-        </View>
             style={{width:210,height:120,
             marginRight:10,borderRadius:7}} /> 
         </TouchableOpacity>
      )}
      />
     </View>
+  )
+}
