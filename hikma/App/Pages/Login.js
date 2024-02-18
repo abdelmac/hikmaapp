ffc77e1601f164504d76e902be00854c48741fbd
abdelmac <abdelkadermachta@gmail.com>
2024-02-18 23:50:09 +0100

    thbth

--- hikma/App/Pages/Login.js
+++ hikma/App/Pages/Login.js
@@ -1,11 +1,17 @@
 import { View, Text, Image, StyleSheet } from 'react-native';
 import React from 'react';
+import Colors from './../Shared/Colors';
 
 export default function Login() {
   return (
     <View>
       <Image source={require('./../../assets/images/login.png')} />
-      <Text style={styles.welcomeText}>Login now </Text>
+      <View style={styles.container}>
+        <Text style={styles.welcomeText}>Start de aventure</Text>
+        <Text style={{textAlign:'center',marginTop:40,fontSize:25}}>Login/Signup</Text>
+        <Text style={styles.button}>Sign in with Google</Text>
+      </View>
+      
       
     </View>
   )
@@ -13,9 +19,21 @@ export default function Login() {
 
 
 const styles = StyleSheet.create({
+  container:{
+    paddingTop:40,
+    marginTop: -20,
+    backgroundColor: '#fff',
+    borderTopRightRadius:20,
+    borderTopLeftRadius:30,
+    borderBottomStartRadius:20,
+  },
   welcomeText:{
     fontSize:35,
     textAlign: 'center',
     fontWeight: 'bold',
-  }
+  },
+  button:{
+    backgroundColor:Colors.primary,
+    textAlign:'center',
+  },
 });
